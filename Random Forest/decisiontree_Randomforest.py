c6b463a4e915c6d1d7bdfa26c253950f3ac45930
SALMAN KHAN <51213314+sk10salman@users.noreply.github.com>
2020-04-05 15:01:43 +0530

    random forest

---
+++ Random Forest/decisiontree_Randomforest.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('Datasets/Social_Network_Ads.csv')
+dataset.head()
+X = dataset.iloc[:,2:4].values
+Y = dataset.iloc[:,-1].values
+# dividing data in train and test 
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.25, random_state =0)
+
+
+# scaling data for right graph not decision tree as it has it in-built
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state=0)
+classifier.fit(X_train,Y_train)
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, y_pred)
+print(cm)
+
+
+
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Now lets implement Random Forest Tree Approach
